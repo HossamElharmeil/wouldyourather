bb85dc0d29278e585bc29c0590752ced639cab92
HossamElharmeil <hossam.elharmil@gmail.com>
2021-07-02 17:22:11 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import TheHeader from './components/TheHeader'
 import TheNavBar from './components/TheNavBar'
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 function App() {
-  return <div>
+  return <>
     <TheNavBar />
-    <TheHeader />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={() => <div>Main</div>} />
+        <Route exact path='/home' component={() => <div>Home</div> } />
+      </Switch>
+    </BrowserRouter>
+    </>
 }
 
 export default App
