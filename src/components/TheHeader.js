812fce29bf094eb96dad23a8b272f10b90d5e9e8
root <hossam.elharmil@gmail.com>
2021-07-02 14:57:29 +0000

    navbar

---
+++ src/components/TheHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TheHeader = () => {
+	return <nav className="nav">
+		<ul className="row">
+      		<li>Home</li>
+  			<li>Leaderboard</li>
+  	   	</ul>
+      </nav>
+}
+
+export default TheHeader
