d3190cdd2256507482618c37811e96acf3d68424
HossamElharmeil <hossam.elharmil@gmail.com>
2021-07-04 23:41:38 +0200

    leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,5 +1,26 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getUsers } from "../redux/actions/usersActions"
+
 const Leaderboard = () => {
-    return <div>Leaderboard</div>
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getUsers())
+    }, [dispatch])
+
+    const users = useSelector(state => state.users)
+    const usersEntries = Object.entries(users)
+    const sortedUsers = usersEntries.sort((a, b) => (b[1].questions.length + Object.keys(b[1].answers).length - a[1].questions.length - Object.keys(a[1].answers).length))
+    console.log(sortedUsers)
+
+    return <div>
+        <ul className='users'>
+            {sortedUsers.map(([key, value]) => <li key={key}>
+                {value.name}
+            </li>)}
+        </ul>
+    </div>
 }
 
 export default Leaderboard
